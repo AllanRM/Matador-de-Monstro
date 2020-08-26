cffcb546e1209b6b87ac972c5e9aebc4e31090b9
Allan R. M <prog03@mastersistemas.net>
2020-08-26 10:38:36 -0300

    Adição de nível de dificuldade

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ new Vue({
         monsterLife: 100,
         curas: 3,
         espec: 3,
-        logs: []
+        logs: [],
+        nivel: 3 // 1-Facil, 2-médio, 3-Impossível
     },
     computed: {
         hasResult () {
@@ -15,24 +16,27 @@ new Vue({
 
     },
     methods: {
-        iniciar () {
+        iniciar (nivel) {
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
             this.curas = 3
             this.espec = 3
             this.logs = []
+            this.nivel = nivel
+            this.log(`Lutar no nível ${nivel}`, 'player')
         },
 
         desistir () {
             this.running = false
             this.playerLife = 100
             this.monsterLife = 100
+            this.logs = []
         },
 
         atacar (especial) {
             if ((!especial) || ((especial) && (!this.espec == 0))) {
-                const forca = this.getRandom(4, especial ? 8 : 6)
+                const forca = this.getRandom((this.nivel == 1 || this.nivel == 2 ? 5 : 4), especial ? 8 : 6)
                 this.monsterLife = Math.max(this.monsterLife - forca, 0)
                 if (this.monsterLife > 0) { // Se o monstro ja morreu nao me contra-ataca
                     this.atacarMonstro()
@@ -45,7 +49,7 @@ new Vue({
         },
 
         atacarMonstro () {
-            const forca = this.getRandom(4, 8)
+            const forca = this.getRandom(4, (this.nivel == 1 ? 6 : 8))
             this.playerLife = Math.max(this.playerLife - forca, 0)
             this.log(`Monstro atingiu o jogador com ${forca}`, 'monster')
         },
@@ -69,7 +73,6 @@ new Vue({
 
         log (text, cls) {
             this.logs.unshift({ text, cls })
-            // console.log(this.logs)
         }
     },
     watch: {
